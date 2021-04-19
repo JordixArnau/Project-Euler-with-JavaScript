25157d832b182d7ec06c6e5b7a5d3c83721399e1
Jordi <aejordi@gmail.com>
2021-04-19 21:08:56 +0200

    Euler2 - Problem 2 resolved

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import {euler1} from './Problem_1/Euler1.js';
+import {euler2} from './Problem_2/Euler2.js';
 
 
-let result1 = euler1(1000);
-document.querySelector('#main__euler1 .result').append(result1);
+document.querySelector('#main__euler1 .result').append(euler1(1000));
+
+document.querySelector('#main__euler2 .result').append(euler2());
